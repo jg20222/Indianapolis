401952ec21bc44780ed80461709fab4f614ed4b4
jg20222 <115832519+jg20222@users.noreply.github.com>
2022-10-15 10:56:20 -0400

    Add files via upload

---
+++ Indianapolis.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[41]:
+
+
+import googlemaps
+
+
+# In[43]:
+
+
+gmaps.geocode("500 W. Washington Street, Indianapolis, IN")
+
+
+# In[44]:
+
+
+gmaps.geocode("650 W. Washington Street, Indianapolis, IN")
+
+
+# In[45]:
+
+
+gmaps.geocode("502 W. Maryland Street, Indianapolis, IN")
+
+
+# In[46]:
+
+
+gmaps.geocode("901 W. New York Street, Indianapolis, IN")
+
+
+# In[47]:
+
+
+gmaps.geocode("4150 Kildeer Drive, Indianapolis, IN")
+
